3bf028af8f9a52b7b873acf23f59aac917bddf53
Cyt b <cytb@users.noreply.github.com>
2015-08-28 10:43:35 +0900

    Created 2015-08-28-0003.hs

---
+++ 2015-08-28-0003.hs
@@ -0,0 +1,12 @@
+
+n = 600851475143
+
+divisible a b = a `mod` b == 0
+primes = 2:[ m | m <- [3..], all (not . divisible m) (takeWhile (< m `div` 2) primes)  ]
+
+factors m
+  | m == 1    = []
+  | otherwise = p:(factors $ m `div` p)
+  where p = head $ dropWhile (not . divisible m) primes
+
+main = putStrLn . show $ factors n
