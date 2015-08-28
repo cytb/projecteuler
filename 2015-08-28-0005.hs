d594d5051dd7322c4408b5ea435f39b7bae79836
Cyt b <cytb@users.noreply.github.com>
2015-08-28 11:40:26 +0900

    Created 2015-08-28-0005.hs

---
+++ 2015-08-28-0005.hs
@@ -0,0 +1,19 @@
+import Data.List
+import Data.Function
+
+divisible a b = a `mod` b == 0
+primes = 2:[ m | m <- [3..], all (not . divisible m) (takeWhile (<= m `div` 2) primes)  ]
+
+factors m
+  | m == 1    = []
+  | otherwise = p:(factors $ m `div` p)
+  where p = head $ dropWhile (not . divisible m) primes
+
+nfactors =   foldl (*) 1
+           . concat . map (\x -> replicate (snd x) (fst x))
+           . map (maximumBy (compare `on` snd))
+           . groupBy ((==) `on` fst) . sortBy (compare `on` fst)
+           . concat . map (map (\x -> (head x, length x) ) . group . factors)
+
+main = putStrLn . show $ nfactors [1..20]
+
