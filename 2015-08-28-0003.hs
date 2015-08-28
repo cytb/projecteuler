c62559f237712c662a53a4c56ef29e74b4dd0143
Cyt b <cytb@users.noreply.github.com>
2015-08-28 12:54:27 +0900

    Updated 2015-08-28-0003.hs

--- 2015-08-28-0003.hs
+++ 2015-08-28-0003.hs
@@ -2,7 +2,7 @@
 n = 600851475143
 
 divisible a b = a `mod` b == 0
-primes = 2:[ m | m <- [3..], all (not . divisible m) (takeWhile (<= m `div` 2) primes)  ]
+primes = 2:[ m | m <- [3,5..], all (not . divisible m) (takeWhile ((<= m).(^2)) primes)  ]
 
 factors m
   | m == 1    = []
